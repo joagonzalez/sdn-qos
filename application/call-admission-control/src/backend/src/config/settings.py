38c5da10c1ebab6f0a9ced8c8db28f3f25dd31af
indica <indica@lagatitadegabo.com>
2018-10-21 04:14:09 -0300

    Backend settings

--- application/call-admission-control/src/backend/src/config/settings.py
+++ application/call-admission-control/src/backend/src/config/settings.py
@@ -1,17 +1,17 @@
 config = {
   'ari': {
-    'host': 'http://0.0.0.0:8088/',
+    'host': 'http://asterisk:8088/',
     'username': 'asterisk',
     'password': 'asterisk',
   },
   'ryu': {
-    'baseurl': 'http://0.0.0.0:8080',
+    'baseurl': 'http://ryu:8080',
   },
   'frontService': {
-    'host': '0.0.0.0',
+    'host': 'call_admission_control_backend',
     'listen': 8000
   },
   'client': {
-    'baseurl': 'ws://0.0.0.0:8000',
+    'baseurl': 'ws://call_admission_control_backend:8000',
   }
 }
