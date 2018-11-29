8331e3cce3577f22c9881096fb998e73903bc096
Ramiro Gonzalez <rgonzalez@makingsense.com>
2018-11-29 18:25:38 -0300

    Agregando gui topology en CAC

--- application/call-admission-control/src/frontend/src/server.js
+++ application/call-admission-control/src/frontend/src/server.js
@@ -5,10 +5,15 @@ let app = express();
 app.set('view engine', 'html');
 app.set('views', 'src');
 
-app.use('/', express.static('dist', { index: false }));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+app.use('/', express.static('GuiTopology', { index: false }));
 
 app.get('*', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '/index.html'));
+    res.sendFile(path.join(__dirname, '/GuiTopology/index.html'));
 });
 app.listen(3000, () => {
     console.log("listening on localhost:3000 (non-universal)");
